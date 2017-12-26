032bbee0538a158f0c2ad27620d69b356e5b2def
wuchong_job <wu.chong313@gmail.com>
2017-12-26 16:29:44 +0800

    map地图

--- src/views/businessMarket/riskMap.vue
+++ src/views/businessMarket/riskMap.vue
@@ -15,82 +15,103 @@
         
 <script>
 // import option from './data/map-options'  // 引入配置项
-import map from './data/map'
-import pie from './data/pie'
-import bar from './data/bar'
-import radar from './data/radar'
-import line from './data/line'
+import map from "./data/map";
+import pie from "./data/pie";
+import bar from "./data/bar";
+import radar from "./data/radar";
+import line from "./data/line";
 // require('./data/china')  // 引入china.js地图文件
-import echarts from 'echarts/lib/echarts'
-import 'echarts/lib/chart/map'
-import 'echarts/lib/chart/pie'
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/chart/radar'
-import 'echarts/lib/chart/line'
+import echarts from "echarts/lib/echarts";
+import "echarts/lib/chart/map";
+import "echarts/lib/chart/pie";
+import "echarts/lib/chart/bar";
+import "echarts/lib/chart/radar";
+import "echarts/lib/chart/line";
 // Map of China
-import chinaMap from './china.json'
+import chinaMap from "./china.json";
 // registering map data
-echarts.registerMap('china', chinaMap)
+echarts.registerMap("china", chinaMap);
 
 export default {
-  name: '',
+  name: "",
   data() {
     return {
       pie,
       bar,
       radar,
       line,
-      items: []
-    }
+      items: [],
+      dealerData: [],
+      mapData: []
+    };
   },
   methods: {
     drawChinaMap() {
-      this.items = document.querySelectorAll('.flex-container .item')
+      this.items = document.querySelectorAll(".flex-container .item");
       for (let i = 0; i < this.items.length; i++) {
-        this.items[i].dataset.order = i + 1
+        this.items[i].dataset.order = i + 1;
       }
       // map
-      var myMap = this.$echarts.init(document.getElementById('mapContainer'))
-      myMap.setOption(map)
+      var myMap = this.$echarts.init(document.getElementById("mapContainer"));
+      var thisMap = map(this.formatMapData(this.data.dealerData, this.data.mapData))
+      myMap.setOption(thisMap);
 
       // pie
-      var myPie = this.$echarts.init(document.getElementById('pieContainer'))
-      myPie.setOption(pie)
+      var myPie = this.$echarts.init(document.getElementById("pieContainer"));
+      myPie.setOption(pie);
 
       // line
-      var myLine = this.$echarts.init(document.getElementById('lineContainer'))
-      myLine.setOption(line)
+      var myLine = this.$echarts.init(document.getElementById("lineContainer"));
+      myLine.setOption(line);
 
       // radar
-      var myRadar = this.$echarts.init(document.getElementById('radarContainer'))
-      myRadar.setOption(radar)
+      var myRadar = this.$echarts.init(
+        document.getElementById("radarContainer")
+      );
+      myRadar.setOption(radar);
     },
     _resize() {
-      this.$root.charts.forEach((myChart) => {
-        myChart.resize()
-      })
+      this.$root.charts.forEach(myChart => {
+        myChart.resize();
+      });
     },
     clickChart(clickIndex) {
-      const activeItem = document.querySelector('.flex-container .active')
-      const activeIndex = activeItem.dataset.order
-      console.log(activeIndex)
-      const clickItem = this.items[clickIndex - 1]
+      const activeItem = document.querySelector(".flex-container .active");
+      const activeIndex = activeItem.dataset.order;
+      console.log(activeIndex);
+      const clickItem = this.items[clickIndex - 1];
       if (activeIndex === clickIndex) {
-        return
+        return;
       }
-      activeItem.classList.remove('active')
-      clickItem.classList.add('active')
-      this._setStyle(clickItem, activeItem)
+      activeItem.classList.remove("active");
+      clickItem.classList.add("active");
+      this._setStyle(clickItem, activeItem);
     },
     _setStyle(el1, el2) {
-      const transform1 = el1.style.transform
-      const transform2 = el2.style.transform
-      el1.style.transform = transform2
-      el2.style.transform = transform1
+      const transform1 = el1.style.transform;
+      const transform2 = el2.style.transform;
+      el1.style.transform = transform2;
+      el2.style.transform = transform1;
+    },
+    /**
+     * 地图数据格式化
+     * @augments {} data
+     * @augments {} map
+     */
+    formatMapData(data, map) {
+      let newData = data.map(item => {
+        return {
+          name: item.name,
+          value: map[item.name]
+            ? map[item.name].concat(item.value)
+            : []
+        };
+      })
+      return newData
     }
   },
   mounted() {
-    this.drawChinaMap()
+    this.drawChinaMap();
     // let dataIndex = -1
     // const pie = this.$refs.pie
     // const dataLen = pie.options.series[0].data.length
@@ -115,21 +136,21 @@ export default {
     // }, 1000)
     // this.connected = true
   }
-}
+};
 </script>
         
 <style scoped>
-.item{
+.item {
   padding: 0px;
   margin: 0px;
   width: 66%;
   height: 91.5%;
-  position:absolute;
-  transform:scale(0.33);
+  position: absolute;
+  transform: scale(0.33);
   text-align: center;
-  transition:all 0.8s;
-  background:rgba(32, 32, 35, 0.5);
-} 
+  transition: all 0.8s;
+  background: rgba(32, 32, 35, 0.5);
+}
 /* .dashboard{
   position:absolute;
   width:100%;
@@ -148,10 +169,10 @@ export default {
   margin:  0 auto 100px auto;
   box-sizing: content-box;
 } */
-.active{
+.active {
   width: 66%;
   height: 94%;
   margin-left: 10px;
-  line-height: 300px;     
+  line-height: 300px;
 }
 </style>
